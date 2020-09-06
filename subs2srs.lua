41f8599e11d0d752db49ed832f56019579be01df
Ren Tatsumoto <tatsu@autistici.org>
2020-09-07 02:23:11 +0300

    pressing 'n' on the menu screen adds a note

--- subs2srs.lua
+++ subs2srs.lua
@@ -739,9 +739,9 @@ menu.keybinds = {
     { key = 'c', fn = function() subs.set_starting_line() end },
     { key = 'r', fn = function() subs.reset_timings() end },
     { key = 'g', fn = function() export_to_anki(true) end },
+    { key = 'n', fn = function() export_to_anki() end },
     { key = 'm', fn = function() update_last_note() end },
     { key = 't', fn = function() clip_autocopy.toggle() end },
-    { key = 'a', fn = function() menu.close() end },
     { key = 'ESC', fn = function() menu.close() end },
 }
 
@@ -765,7 +765,8 @@ menu.update = function()
     osd:tab():bold('s: '):append('Set start time to current position'):newline()
     osd:tab():bold('e: '):append('Set end time to current position'):newline()
     osd:tab():bold('r: '):append('Reset timings'):newline()
-    osd:tab():bold('g: '):append('Export note using the `Add Cards` dialog'):newline()
+    osd:tab():bold('n: '):append('Export note'):newline()
+    osd:tab():bold('g: '):append('Export note using the `Add Cards` GUI'):newline()
     osd:tab():bold('m: '):append('Add audio and image to the last added note'):newline()
     osd:tab():bold('t: '):append('Toggle clipboard autocopy'):newline()
     osd:tab():bold('ESC: '):append('Close'):newline()
