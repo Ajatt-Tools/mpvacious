97f961e671bc734d392f042e9b227042a8364635
evakuator <kuatabylkasymov@gmail.com>
2023-08-15 21:28:57 +0600

    string.lower() is preferrable

--- utils/filename_factory.lua
+++ utils/filename_factory.lua
@@ -80,7 +80,7 @@ local make_filename = function(...)
     local args = {...}
     local timestamp = #args < 3 and timestamp_static(...) or timestamp_range(...)
     local filename_ = anki_compatible_length(filename, timestamp) .. timestamp
-    filename_ = filename_:lower()
+    filename_ = string.lower(filename_)
     return filename_
 end
 
