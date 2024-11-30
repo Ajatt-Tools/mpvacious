6dfe1e30cd01c9d8409176d3ea1b69d0b6b1af49
Ren Tatsumoto <tatsu@autistici.org>
2024-11-30 17:34:50 +0300

    use a random tmp file for each curl request

--- platform/win.lua
+++ platform/win.lua
@@ -9,12 +9,7 @@ local mp = require('mp')
 local h = require('helpers')
 local utils = require('mp.utils')
 local base64 = require('utils.base64')
-local curl_tmpfile_path = utils.join_path(os.getenv('TEMP'), 'curl_tmp.txt')
 local self = { windows = true, healthy = true, clip_util = "cmd", }
-
-mp.register_event('shutdown', function()
-    os.remove(curl_tmpfile_path)
-end)
 
 self.tmp_dir = function()
     return os.getenv('TEMP')
@@ -30,7 +25,20 @@ self.copy_to_clipboard = function(text)
     )
 end
 
+self.gen_random_tmp_file_path = function()
+    return utils.join_path(self.tmp_dir(), string.format('curl_tmp_%d.txt', math.random(10^9)))
+end
+
+self.gen_unique_tmp_file_path = function()
+    local curl_tmpfile_path = self.gen_random_tmp_file_path()
+    while h.file_exists(curl_tmpfile_path) do
+        curl_tmpfile_path = self.gen_random_tmp_file_path()
+    end
+    return curl_tmpfile_path
+end
+
 self.curl_request = function(url, request_json, completion_fn)
+    local curl_tmpfile_path = self.gen_unique_tmp_file_path()
     local handle = io.open(curl_tmpfile_path, "w")
     handle:write(request_json)
     handle:close()
@@ -45,7 +53,11 @@ self.curl_request = function(url, request_json, completion_fn)
         '--data-binary',
         table.concat { '@', curl_tmpfile_path }
     }
-    return h.subprocess(args, completion_fn)
+    local function wrap()
+        completion_fn()
+        os.remove(curl_tmpfile_path)
+    end
+    return h.subprocess(args, wrap)
 end
 
 return self
