f12466f4db00e126371676309e5e0a7d4ecc78c6
Ren Tatsumoto <tatsu@autistici.org>
2026-01-27 07:30:02 +0300

    update header

--- osd_styler.lua
+++ osd_styler.lua
@@ -1,21 +1,9 @@
 --[[
+Copyright: Ajatt-Tools and contributors; https://github.com/Ajatt-Tools
+License: GNU GPL, version 3 or later; http://www.gnu.org/licenses/gpl.html
+
 A helper class for styling OSD messages
 http://docs.aegisub.org/3.2/ASS_Tags/
-
-Copyright (C) 2021 Ren Tatsumoto
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ]]
 
 local OSD = {}
