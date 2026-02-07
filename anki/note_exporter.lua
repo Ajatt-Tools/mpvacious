22850d6bb64d2a8539f67ed22db13452e6fb4212
Ren Tatsumoto <tatsu@autistici.org>
2026-02-07 23:14:25 +0300

    replace sub call with str_limit
    handle utf8 correctly

--- anki/note_exporter.lua
+++ anki/note_exporter.lua
@@ -141,7 +141,7 @@ local function make_exporter()
             if not h.is_empty(field_data) then
                 local max_len = 20
                 if string.len(field_data) > max_len then
-                    field_data = field_data:sub(1, max_len) .. "…"
+                    field_data = h.str_limit(max_len)
                 end
                 h.notify(string.format("Updated note: %s.", field_data))
             else
