8b328094ba64c253b2b0af4de730cf51d328430b
Ren Tatsumoto <tatsu@autistici.org>
2026-02-10 06:22:24 +0300

    update default config

--- mpvacious/config/defaults.lua
+++ mpvacious/config/defaults.lua
@@ -75,7 +75,7 @@ this.defaults = {
     show_selected_text = true,
 
     -- Make sure to remove loudnorm from ffmpeg_audio_args and mpv_audio_args before enabling.
-    loudnorm = false,
+    loudnorm = true,
     loudnorm_target = -16,
     loudnorm_range = 11,
     loudnorm_peak = -1.5,
@@ -86,8 +86,10 @@ this.defaults = {
     -- Better to remove loudnorm from custom args and enable two-pass loudnorm.
     -- Enabling loudnorm both through the separate switch and through custom args
     -- can lead to unpredictable results.
-    ffmpeg_audio_args = '-af loudnorm=I=-16:TP=-1.5:LRA=11:dual_mono=true',
-    mpv_audio_args = '--af-append=loudnorm=I=-16:TP=-1.5:LRA=11:dual_mono=true',
+    ffmpeg_audio_args = '',
+    mpv_audio_args = '',
+    --ffmpeg_audio_args = '-af loudnorm=I=-16:TP=-1.5:LRA=11:dual_mono=true',
+    --mpv_audio_args = '--af-append=loudnorm=I=-16:TP=-1.5:LRA=11:dual_mono=true',
 
     -- Anki
     create_deck = false,               -- automatically create a deck for new cards
